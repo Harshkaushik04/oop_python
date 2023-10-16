68013469c5201d69ef266121d601790ddafd10b7
Harshkaushik04 <2023AIB1006@iitrpr.ac.in>
2023-10-16 18:13:38 +0530

    learning oop in python

---
+++ oop_part_1.py
@@ -0,0 +1,23 @@
+#generality to specifity
+#making custom datatype
+#class is a blueprint
+#only list can access functions defined in class list
+#class always consists of only 2 things:1.data[property] 2.functions[behaviour]
+'''
+class Car:
+    color="blue" #data
+    model="sports" #data
+    def calculate_avg_speed(km,time): #function
+        #some code
+'''
+#class name should be in Pascal case-ThisIsPascalCase
+#this_is_snake_case
+#thisIsCamelCase
+#plus notation for public and minus notation for private
+'''
+object defining:
+Car(class)-------->wagonr(object)
+wagonr=Car()
+'''
+#for inbuilt classes of python we use object literal which means easy method to create object
+#for example for list class, we make object as L=[] instead of the traditional way(L=list())
